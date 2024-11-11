032d0e6c5dec15e1eff06fd3091113f4769f7d7d
benard <benardboiwo703@gmail.com>
2024-11-11 14:56:45 +0300

    initial commit

---
+++ lost-and-found/src/Found item card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LostItemList from './Components/LostItemList'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Lost Items</h1>
+      <LostItemList />
+    </div>
+  )
+}
+
+export default App
+
